817266717acbcf557dc3bb2ceb5d532f5e149272
stebin96 <stebinboban96@gmail.com>
2025-11-01 11:55:26 +0530

    Added getAllHotels route

--- server/routes/hotelRoutes.js
+++ server/routes/hotelRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerHotel } from "../controllers/hotelController.js";
+import { registerHotel, getAllHotels } from "../controllers/hotelController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const hotelRouter = express.Router();
